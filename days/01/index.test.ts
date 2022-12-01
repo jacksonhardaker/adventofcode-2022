771b32b2ebabacfe8a21c25a5683854d6569bbf6
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-01 08:39:51 -0500

    add day 1

--- days/01/index.test.ts
+++ days/01/index.test.ts
@@ -1,6 +1,28 @@
 import { expect, test, describe } from 'bun:test';
 import * as dayModule from './index';
 
-describe('', () => {
-  test('', () => {});
+
+const input = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+describe('day 1', () => {
+  test('part 1', () => {
+    expect(dayModule.part1(input)).toBe(24000);
+  });
+
+  test('part 2', () => {
+    expect(dayModule.part2(input)).toBe(45000);
+  });
 });
--- days/01/index.ts
+++ days/01/index.ts
@@ -1,3 +1,26 @@
 import { readInput } from 'utils/readInput';
 
 const input = readInput(import.meta.dir);
+
+export const part1 = (input: string) => {
+  const elves = input
+    .split(/\n\n/)
+    .map((elf: string) => elf.split(/\n/).map(Number));
+
+  return Math.max(...elves.map((elf) => elf.reduce((sum, num) => sum + num)));
+};
+
+export const part2 = (input: string) => {
+  const elves = input
+    .split(/\n\n/)
+    .map((elf: string) => elf.split(/\n/).map(Number));
+
+  return elves
+    .map((elf) => elf.reduce((sum, num) => sum + num))
+    .sort((a, b) => a > b ? -1 : 1)
+    .slice(0, 3)
+    .reduce((sum, num) => sum + num);
+};
+
+console.log(`Part 1: ${part1(input)}`);
+console.log(`Part 2: ${part2(input)}`);
