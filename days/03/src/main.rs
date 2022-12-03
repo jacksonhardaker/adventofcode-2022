1206120f9b932c61bd5ea5f0e09112db4169e823
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-03 09:43:41 -0500

    day 3 part 2

--- days/03/src/main.rs
+++ days/03/src/main.rs
@@ -27,20 +27,47 @@ fn part1(input: &str, alphabet: Vec<String>) {
             let index = alphabet.iter().position(|r| r.eq(c)).unwrap();
             return (index + 1) as i32;
         })
-        .collect::<Vec<i32>>().iter().sum::<i32>();
-
+        .collect::<Vec<i32>>()
+        .iter()
+        .sum::<i32>();
 
     println!("Part 1: {}", total);
 }
 
-fn _part2() {}
+fn part2(input: &str, alphabet: Vec<String>) {
+    let lines: Vec<&str> = input.split("\n").collect();
+
+    let mut total = 0;
+    let mut i = 0;
+    while i < lines.len() {
+        let first = lines[i];
+        let second = lines[i + 1];
+        let third = lines[i + 2];
+
+        // Find intersection of strings
+        first.chars().any(|c| {
+            if second.contains(c) && third.contains(c) {
+                let index = alphabet.iter().position(|r| r.eq(&c.to_string())).unwrap();
+                total += index + 1;
+                // return (index + 1) as i32;
+                return true;
+            }
+            return false;
+        });
+
+        i += 3;
+    }
+    println!("Part 2: {}", total);
+}
 
 fn main() {
     let input = fs::read_to_string("./days/03/input.txt").expect("Error!");
+
     // Create Vec of priority values where index + 1 is priority.
     let mut alphabet: Vec<String> = ('a'..='z').map(|c| c.to_string()).collect();
     let alphabet_upper: Vec<String> = ('A'..='Z').map(|c| c.to_string()).collect();
     alphabet.extend(alphabet_upper.iter().cloned());
 
-    part1(input.trim(), alphabet);
+    part1(input.trim(), alphabet.clone());
+    part2(input.trim(), alphabet);
 }
