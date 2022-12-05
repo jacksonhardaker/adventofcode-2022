ff7c0ec52a71f342fe7ca78d7e87a773d50a98bd
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-05 06:08:05 -0500

    create day 05

---
+++ days/05/index.ts
@@ -0,0 +1,9 @@
+import { readInput } from 'utils/readInput';
+
+const rawInput = readInput(import.meta.dir);
+
+export const part1 = (input: string) => {};
+export const part2 = (input: string) => {};
+
+// console.log(`Part 1: ${part1(rawInput)}`);
+// console.log(`Part 2: ${part2(rawInput)}`);
