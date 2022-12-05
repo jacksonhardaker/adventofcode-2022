c793f54348928a413eb9337af0a6818e22d933ba
Jackson Hardaker <jacksonhardaker@lyft.com>
2022-12-05 11:29:28 -0500

    cleanup and remove ts

--- utils/readInput.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-
-export const readInput = (baseDir: string) => {
-  return readFileSync(resolve(baseDir, 'input.txt'), {
-    encoding: 'utf-8',
-  }).trimEnd();
-};
