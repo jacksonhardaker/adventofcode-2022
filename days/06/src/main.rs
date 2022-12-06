bb739077ba8a7b275f8022c2c9d31a3da4c51edb
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-06 06:36:21 -0500

    add tests

--- days/06/src/main.rs
+++ days/06/src/main.rs
@@ -1,7 +1,7 @@
 use std::collections::{HashSet, VecDeque};
 use std::fs;
 
-fn part1(datastream: String) {
+fn part1(datastream: String) -> usize {
     let mut last_four: VecDeque<char> = VecDeque::new();
     let mut marker_index: usize = 0;
     for (index, char) in datastream.chars().enumerate() {
@@ -20,8 +20,9 @@ fn part1(datastream: String) {
         }
     }
 
-    println!("Last four: {:#?}", last_four);
-    println!("Part 1: {}", marker_index + 1);
+    // println!("Last four: {:#?}", last_four);
+
+    marker_index + 1
 }
 
 fn _part2() {}
@@ -29,10 +30,14 @@ fn _part2() {}
 fn main() {
     let _input = fs::read_to_string("./days/06/input.txt").expect("Error!");
 
-    // part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_owned());
-    // part1("bvwbjplbgvbhsrlpgdmjqwftvncz".to_owned());
-    // part1("nppdvjthqldpwncqszvftbrmjlhg".to_owned());
-    // part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_owned());
-    // part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_owned());
-    part1(_input.trim().to_string());
+    println!("Part 1: {}", part1(_input.trim().to_string()));
+}
+
+#[test]
+fn test_part1() {
+    assert_eq!(part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb".to_owned()), 7);
+    assert_eq!(part1("bvwbjplbgvbhsrlpgdmjqwftvncz".to_owned()), 5);
+    assert_eq!(part1("nppdvjthqldpwncqszvftbrmjlhg".to_owned()), 6);
+    assert_eq!(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".to_owned()), 10);
+    assert_eq!(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".to_owned()), 11);
 }
