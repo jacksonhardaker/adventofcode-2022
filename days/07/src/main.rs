bfac9bb5078c6b106db5878c050dfc19d04746f4
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-07 21:43:34 -0500

    day 07 part 2

--- days/07/src/main.rs
+++ days/07/src/main.rs
@@ -1,7 +1,7 @@
 use regex::Regex;
 use std::{collections::HashMap, fs, str::Split};
 
-fn part1(input: Split<&str>) -> usize {
+fn get_dirs(input: Split<&str>) -> HashMap<String, usize> {
     let mut dirs: HashMap<String, usize> = HashMap::new();
     let mut current_path: Vec<String> = Vec::new();
 
@@ -50,16 +50,32 @@ fn part1(input: Split<&str>) -> usize {
         }
     });
 
+    dirs
+}
+
+fn part1(input: Split<&str>) -> usize {
+    let dirs = get_dirs(input);
     dirs.into_values().filter(|size| size <= &100000).sum()
 }
 
-fn _part2() {}
+fn part2(input: Split<&str>) -> usize {
+    let total_disk_space: usize = 70000000;
+    let required_space: usize = 30000000;
+    let dirs = get_dirs(input);
+    let current_unused = total_disk_space - dirs.get("/").unwrap();
+
+    dirs.into_values()
+        .filter(|size| (current_unused + size) >= required_space)
+        .min()
+        .unwrap()
+}
 
 fn main() {
     let raw_input = fs::read_to_string("./days/07/input.txt").expect("Error!");
     let input = raw_input.trim().split("\n");
 
-    println!("Part 1: {}", part1(input));
+    println!("Part 1: {}", part1(input.clone()));
+    println!("Part 2: {}", part2(input.clone()));
 }
 
 #[test]
@@ -67,4 +83,11 @@ fn test_part1() {
     let raw_input = fs::read_to_string("./test-input.txt").expect("Error!");
     let input = raw_input.trim().split("\n");
     assert_eq!(part1(input), 95437);
+}
+
+#[test]
+fn test_part2() {
+    let raw_input = fs::read_to_string("./test-input.txt").expect("Error!");
+    let input = raw_input.trim().split("\n");
+    assert_eq!(part2(input), 24933642);
 }
