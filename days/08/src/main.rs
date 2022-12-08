83d14acb001da1508a9b00aa6e3e22b058eb4cfb
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-08 08:00:47 -0500

    day 08 part 2

--- days/08/src/main.rs
+++ days/08/src/main.rs
@@ -1,15 +1,19 @@
 use std::{fs, str::Split};
 
-fn part1(input: Split<&str>) -> usize {
-    let mut visible_trees = 0;
-
-    let grid: Vec<Vec<usize>> = input
+fn build_tree_grid(input: Split<&str>) -> Vec<Vec<usize>> {
+    input
         .map(|line| {
             line.chars()
                 .map(|char| char.to_string().parse::<usize>().unwrap())
                 .collect()
         })
-        .collect();
+        .collect()
+}
+
+fn part1(input: Split<&str>) -> usize {
+    let mut visible_trees = 0;
+
+    let grid = build_tree_grid(input);
     let lines_len = grid.len();
 
     for (i, line) in grid.iter().enumerate() {
@@ -17,7 +21,6 @@ fn part1(input: Split<&str>) -> usize {
 
         for (j, height) in line.iter().enumerate() {
             let is_edge = i == 0 || i == lines_len - 1 || j == 0 || j == chars_len - 1;
-            // println!("{}, {} - {} - {}", i, j, height, is_edge);
 
             if is_edge {
                 // All edges are visible
@@ -45,7 +48,100 @@ fn part1(input: Split<&str>) -> usize {
 }
 
 fn part2(input: Split<&str>) -> usize {
-    0
+    let mut max_view = 0;
+    let grid = build_tree_grid(input);
+    let y_limit = grid.len();
+
+    for (y, line) in grid.iter().enumerate() {
+        let x_limit = line.len();
+
+        for (x, target_height) in line.iter().enumerate() {
+            let mut up_blocked = false;
+            let up_view = if y == 0 {
+                0
+            } else {
+                (0..=y - 1)
+                    .rev()
+                    .map(|yy| {
+                        if up_blocked {
+                            return 0;
+                        }
+
+                        if grid[yy][x] >= *target_height {
+                            up_blocked = true;
+                        }
+
+                        1
+                    })
+                    .sum()
+            };
+            let mut down_blocked = false;
+            let down_view = if y == y_limit - 1 {
+                0
+            } else {
+                (y + 1..y_limit)
+                    .map(|yy| {
+                        if down_blocked {
+                            return 0;
+                        }
+
+                        if grid[yy][x] >= *target_height {
+                            down_blocked = true;
+                        }
+
+                        1
+                    })
+                    .sum()
+            };
+            let mut left_blocked = false;
+            let left_view = if x == 0 {
+                0
+            } else {
+                (0..=x - 1)
+                    .rev()
+                    .map(|xx| {
+                        if left_blocked {
+                            return 0;
+                        }
+
+                        if grid[y][xx] >= *target_height {
+                            left_blocked = true;
+                        }
+
+                        1
+                    })
+                    .sum()
+            };
+            let mut right_blocked = false;
+            let right_view = if x == x_limit - 1 {
+                0
+            } else {
+                (x + 1..x_limit)
+                    .map(|xx| {
+                        if right_blocked {
+                            return 0;
+                        }
+
+                        if grid[y][xx] >= *target_height {
+                            right_blocked = true;
+                        }
+
+                        1
+                    })
+                    .sum()
+            };
+
+            let current_view = up_view * down_view * left_view * right_view;
+            // println!("pos: {}/{}, val: {}, up_view: {}, down_view: {}, left_view: {}, right_view: {}, view: {}", y, x, target_height, up_view, down_view, left_view, right_view, current_view);
+            max_view = if current_view > max_view {
+                current_view
+            } else {
+                max_view
+            };
+        }
+    }
+
+    max_view
 }
 
 fn main() {
@@ -62,9 +158,8 @@ fn test_part1() {
     assert_eq!(part1(input), 21);
 }
 #[test]
-#[ignore]
 fn test_part2() {
     let raw_input = fs::read_to_string("./test-input.txt").expect("Error!");
     let input = raw_input.trim().split("\n");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 8);
 }
