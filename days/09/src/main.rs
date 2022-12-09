7bcf3804152c16aa539b46041230fb7d30d51c3e
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-09 08:02:14 -0500

    day 09 part 2
    first test for part 2 passing
    
    part 2 almost there
    
    refactor for simplicity
    
    simplify more
    
    more cleanup
    
    more cleanup
    
    remove comment
    
    complete part 2
    
    cleanup code

--- days/09/src/main.rs
+++ days/09/src/main.rs
@@ -1,64 +1,6 @@
-use std::{collections::HashSet, fs, str::Split};
+use std::{collections::HashMap, collections::HashSet, fs, str::Split};
 
 type Coords = (i32, i32);
-
-fn part1(input: Split<&str>) -> usize {
-    let mut visited_map: HashSet<Coords> = HashSet::new();
-
-    let mut head: Coords = (0, 0);
-    let mut tail: Coords = (0, 0);
-    visited_map.insert(tail);
-
-    input.for_each(|line| {
-        let mut parts = line.split(" ");
-        let direction = parts.next().unwrap();
-        let distance = parts.next().unwrap().parse::<i32>().unwrap();
-
-        if direction == "U" {
-            // move up by distance
-            let mut moved = 0;
-            while moved != distance {
-                head.1 += 1;
-                tail = calc_tail_pos(head, tail);
-                visited_map.insert(tail);
-                moved += 1;
-            }
-        } else if direction == "D" {
-            // move down by distance
-            let mut moved = 0;
-            while moved != distance {
-                head.1 -= 1;
-                tail = calc_tail_pos(head, tail);
-                visited_map.insert(tail);
-                moved += 1;
-            }
-        } else if direction == "L" {
-            // move left by distance
-            let mut moved = 0;
-            while moved != distance {
-                head.0 += 1;
-                tail = calc_tail_pos(head, tail);
-                visited_map.insert(tail);
-                moved += 1;
-            }
-        } else if direction == "R" {
-            // move right by distance
-            let mut moved = 0;
-            while moved != distance {
-                head.0 -= 1;
-                tail = calc_tail_pos(head, tail);
-                visited_map.insert(tail);
-                moved += 1;
-            }
-        }
-    });
-
-    visited_map.len()
-}
-
-fn part2(input: Split<&str>) -> usize {
-    0
-}
 
 fn calc_tail_pos(head: Coords, tail: Coords) -> Coords {
     let same_x = head.0.abs_diff(tail.0) == 0;
@@ -75,9 +17,9 @@ fn calc_tail_pos(head: Coords, tail: Coords) -> Coords {
     // H and T are on the same x-axis
     if same_x {
         let y = if head.1 - tail.1 > 0 {
-            head.1 - 1
+            tail.1 + 1
         } else {
-            head.1 + 1
+            tail.1 - 1
         };
         return (tail.0, y);
     }
@@ -85,9 +27,9 @@ fn calc_tail_pos(head: Coords, tail: Coords) -> Coords {
     // H and T are on the same y-axis
     if same_y {
         let x = if head.0 - tail.0 > 0 {
-            head.0 - 1
+            tail.0 + 1
         } else {
-            head.0 + 1
+            tail.0 - 1
         };
         return (x, tail.1);
     }
@@ -106,6 +48,96 @@ fn calc_tail_pos(head: Coords, tail: Coords) -> Coords {
 
     (x, y)
 }
+
+fn flip_tail(rope: &mut Vec<Coords>, visited_map: &mut HashSet<(i32, i32)>) {
+    for i in 1..rope.len() {
+        let prev = *rope.get(i - 1).unwrap();
+        rope.get_mut(i)
+            .map(|knot| *knot = calc_tail_pos(prev, *knot));
+        visited_map.insert(rope[rope.len() - 1]);
+    }
+}
+
+fn simulate_rope(input: Split<&str>, rope_len: usize) -> usize {
+    let dir_mod = HashMap::from([("U", 1), ("D", -1), ("L", -1), ("R", 1)]);
+    let mut visited_map: HashSet<Coords> = HashSet::new();
+    let mut rope: Vec<Coords> = (0..rope_len).map(|_| (0, 0)).collect();
+
+    visited_map.insert(rope[rope.len() - 1]);
+
+    input.for_each(|line| {
+        let mut parts = line.split(" ");
+        let direction = parts.next().unwrap();
+        let distance = parts.next().unwrap().parse::<i32>().unwrap();
+
+        for _ in 0..distance {
+            rope.get_mut(0).map(|head| {
+                if direction == "U" || direction == "D" {
+                    head.1 += dir_mod[direction]
+                } else if direction == "L" || direction == "R" {
+                    head.0 += dir_mod[direction]
+                }
+            });
+            flip_tail(&mut rope, &mut visited_map);
+        }
+    });
+
+    visited_map.len()
+}
+
+fn part1(input: Split<&str>) -> usize {
+    simulate_rope(input, 2)
+}
+
+fn part2(input: Split<&str>) -> usize {
+    simulate_rope(input, 10)
+}
+
+/**
+ * Start Helpers
+ */
+fn _print_rope(rope: Vec<Coords>) {
+    for y in (-5..=15).rev() {
+        for x in -11..=14 {
+            let mut knotted = false;
+            for (i, knot) in rope.iter().enumerate() {
+                if knot.0 == x && knot.1 == y {
+                    if i == 0 {
+                        print!("H");
+                    } else {
+                        print!("{}", i)
+                    }
+                    knotted = true;
+                    break;
+                }
+            }
+
+            if !knotted {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+    print!("\n");
+}
+
+fn _print_visited_map(map: HashSet<Coords>) {
+    for y in (-5..=15).rev() {
+        for x in -11..=14 {
+            if map.contains(&(x, y)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        print!("\n");
+    }
+    print!("\n");
+}
+
+/**
+ * End Helpers
+ */
 
 fn main() {
     let input = fs::read_to_string("./days/09/input.txt").expect("Error!");
@@ -132,8 +164,15 @@ fn test_part1() {
     assert_eq!(part1(input), 13);
 }
 #[test]
-fn test_part2() {
+fn test_part2_a() {
     let raw_input = fs::read_to_string("./test-input.txt").expect("Error!");
     let input = raw_input.trim().split("\n");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 1);
+}
+
+#[test]
+fn test_part2_b() {
+    let raw_input = fs::read_to_string("./test-input-2.txt").expect("Error!");
+    let input = raw_input.trim().split("\n");
+    assert_eq!(part2(input), 36);
 }
