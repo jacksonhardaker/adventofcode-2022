4c4a7e4e248d2fee0cfd9c4fc98944e6fe1e9d44
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-10 07:22:00 -0500

    day 10 part 2
    refactor part 1 to reuse
    
    complete part 2

--- days/10/src/main.rs
+++ days/10/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, fs, str::Split};
 
-fn part1(input: Split<&str>) -> i32 {
+fn get_sprite_positions_per_cycle(input: Split<&str>, cycles: i32) -> HashMap<i32, i32> {
     let mut register: HashMap<i32, i32> = HashMap::new();
     let mut x = 1;
     let mut index = 0;
@@ -8,17 +8,9 @@ fn part1(input: Split<&str>) -> i32 {
 
     let mut buffer: Vec<i32> = vec![];
 
-    for cycle in 1..=220 {
-        if cycle == 20
-            || cycle == 60
-            || cycle == 100
-            || cycle == 140
-            || cycle == 180
-            || cycle == 220
-        {
-            // Record x
-            register.insert(cycle, cycle * x);
-        }
+    for cycle in 1..=cycles {
+        // Record x
+        register.insert(cycle, x);
 
         // println!("Cycle: {}, X: {}", cycle, x);
 
@@ -40,18 +32,49 @@ fn part1(input: Split<&str>) -> i32 {
         }
     }
 
-    register.values().sum::<i32>()
+    register
 }
 
-fn part2(input: Split<&str>) -> usize {
-    0
+fn part1(input: Split<&str>) -> i32 {
+    let register = get_sprite_positions_per_cycle(input, 220);
+
+    vec![20, 60, 100, 140, 180, 220]
+        .iter()
+        .map(|c| c * register.get(c).unwrap())
+        .sum()
+}
+
+fn part2(input: Split<&str>) -> Result<(), ()> {
+    let cycle_register = get_sprite_positions_per_cycle(input, 240);
+    let mut sprite_pos = 0..=2;
+
+    let mut cycle = 1;
+    for _ in 1..=6 {
+        println!("");
+        for col in 0..40 {
+            let x = cycle_register.get(&cycle).unwrap();
+            sprite_pos = x - 1..=x + 1;
+
+            if sprite_pos.contains(&col) {
+                print!("#");
+            } else {
+                print!(".")
+            }
+
+            // fin
+            cycle += 1;
+        }
+    }
+
+    Ok(())
 }
 
 fn main() {
     let input = fs::read_to_string("./days/10/input.txt").expect("Error!");
 
     println!("Part 1: {}", part1(input.trim().split("\n")));
-    println!("Part 2: {}", part2(input.trim().split("\n")));
+    println!("Part 2:");
+    part2(input.trim().split("\n")).expect("Oops");
 }
 
 #[test]
@@ -64,5 +87,5 @@ fn test_part1() {
 fn test_part2() {
     let raw_input = fs::read_to_string("./test-input.txt").expect("Error!");
     let input = raw_input.trim().split("\n");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), Ok(()));
 }
