0adc0033561616c0ab0c459ec96623f42b05419e
Jackson Hardaker <jackson.hardaker@gmail.com>
2022-12-12 21:21:44 -0500

    day 12 part 2

--- days/12/src/main.rs
+++ days/12/src/main.rs
@@ -1,11 +1,11 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, VecDeque},
     fs,
 };
 
 const INFINITY: usize = 4294967295;
 
-type Graph = Vec<Vec<char>>;
+type Graph = HashMap<Coords, Node>;
 type Row = usize;
 type Col = usize;
 type Coords = (Row, Col);
@@ -15,8 +15,16 @@ struct HeightMapData {
     graph: Graph,
     src: Coords,
     dest: Coords,
-    initial_unvisited: HashSet<Coords>,
-    initial_tentative_distances: HashMap<Coords, usize>,
+}
+
+#[derive(Debug)]
+struct Node {
+    coords: Coords,
+    char: char,
+    up: Option<Coords>,
+    down: Option<Coords>,
+    left: Option<Coords>,
+    right: Option<Coords>,
 }
 
 fn get_char_value(char: char) -> char {
@@ -32,130 +40,151 @@ fn get_char_value(char: char) -> char {
 /**
  * Dijkstra
  */
-fn find_paths(data: HeightMapData) -> HashMap<Coords, usize> {
-    let mut unvisited = data.initial_unvisited.clone();
-    let mut tentative_dist = data.initial_tentative_distances.clone();
+fn find_shortest_path(data: HeightMapData, start: Coords) -> usize {
+    println!("finding shortest path from {:#?}", start);
+    //   Declare a queue and insert the starting vertex.
+    let mut queue: VecDeque<Coords> = VecDeque::new();
 
-    let mut node = data.src;
-    while unvisited.len() > 0 {
-        println!("{}", unvisited.len());
-        let val = get_char_value(data.graph[node.0][node.1]);
+    // Initialize a visited array and mark the starting vertex as visited.
+    let mut visited: HashSet<Coords> = HashSet::new();
+    let mut distance: HashMap<Coords, usize> = HashMap::new();
 
-        if node == data.dest {
+    visited.insert(start);
+    queue.push_back(start);
+    distance.insert(start, 0);
+
+    // Follow the below process till the queue becomes empty:
+    while queue.len() > 0 {
+        // Remove the first vertex of the queue.
+        let node_coords = queue.pop_front().unwrap();
+        let node = data.graph.get(&node_coords).unwrap();
+        let char_val = get_char_value(node.char);
+
+        // Mark that vertex as visited.
+        visited.insert(node_coords);
+
+        if node_coords == data.dest {
             break;
         }
 
-        // Calculate tentative dist for up,down,left,right
-        let up = if node.0 > 0 {
-            Some((node.0 - 1, node.1))
-        } else {
-            None
-        };
-        let down = if node.0 < data.graph.len() - 1 {
-            Some((node.0 + 1, node.1))
-        } else {
-            None
-        };
-        let left = if node.1 > 0 {
-            Some((node.0, node.1 - 1))
-        } else {
-            None
-        };
-        let right = if node.1 < data.graph[0].len() - 1 {
-            Some((node.0, node.1 + 1))
-        } else {
-            None
-        };
+        // Insert all the unvisited neighbours of the vertex into the queue.
+        vec![node.up, node.down, node.left, node.right]
+            .iter()
+            .for_each(|sibling| {
+                if !sibling.is_none() {
+                    let sib_coords = sibling.unwrap();
 
-        vec![up, down, left, right].iter().for_each(|adj_node| {
-            if !adj_node.is_none() {
-                let adj_coords = adj_node.unwrap();
-                if unvisited.contains(&adj_coords) {
-                    let adj_val = get_char_value(data.graph[adj_coords.0][adj_coords.1]);
+                    if !visited.contains(&sib_coords) {
+                        let sib_char_val =
+                            get_char_value(data.graph.get(&sib_coords).unwrap().char);
 
-                    if adj_val <= val || adj_val as u8 == val as u8 + 1 {
-                        // if adj_val == val || adj_val as u8 == val as u8 - 1 || adj_val as u8 == val as u8 + 1 {
-                        // We can visit from here.
-                        let adj_tent = tentative_dist.get(&adj_coords).unwrap();
-                        let cur_tent = tentative_dist.get(&node).unwrap();
-                        let new_tent = if adj_tent < &(cur_tent + 1) {
-                            adj_tent.to_owned()
-                        } else {
-                            (cur_tent + 1).clone()
-                        };
-
-                        tentative_dist.insert(adj_coords, new_tent);
+                        if sib_char_val <= char_val || sib_char_val as u8 == char_val as u8 + 1 {
+                            distance.insert(sib_coords, *distance.get(&node_coords).unwrap() + 1);
+                            visited.insert(sib_coords);
+                            queue.push_back(sib_coords);
+                        }
                     }
                 }
-            }
-        });
-
-        // Of the unvisited nodes, visit the one with the lowest tentative distance, and remove the current node from unvisited
-        unvisited.remove(&node);
-
-        let mut lowest_tentative = INFINITY;
-        unvisited.iter().for_each(|unvisited_node| {
-            let tent = tentative_dist.get(unvisited_node).unwrap();
-
-            if tent < &lowest_tentative {
-                lowest_tentative = tent.to_owned();
-                node = *unvisited_node;
-            }
-        });
+            })
     }
 
-    tentative_dist
+    let result = distance.get(&data.dest);
+
+    if result.is_none() {
+        INFINITY
+    } else {
+        *result.unwrap()
+    }
 }
 
 fn graph_from_input(input: String) -> HeightMapData {
-    let mut unvisited: HashSet<Coords> = HashSet::new();
-    let mut tenative_distances: HashMap<Coords, usize> = HashMap::new();
     let mut src = (INFINITY, INFINITY);
     let mut dest = (INFINITY, INFINITY);
-    let graph = input
-        .trim()
-        .split("\n")
-        .enumerate()
-        .map(|(row_index, row)| {
-            row.chars()
-                .enumerate()
-                .map(|(col_index, c)| {
-                    if c == 'S' {
-                        src = (row_index, col_index);
-                    }
-                    if c == 'E' {
-                        dest = (row_index, col_index);
-                    }
+    let mut graph: Graph = HashMap::new();
 
-                    unvisited.insert((row_index, col_index));
-                    tenative_distances
-                        .insert((row_index, col_index), if c == 'S' { 0 } else { INFINITY });
+    let rows = input.trim().split("\n").collect::<Vec<&str>>();
+    for (row, line) in rows.iter().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            if c == 'S' {
+                src = (row, col);
+            }
+            if c == 'E' {
+                dest = (row, col);
+            }
 
-                    c
-                })
-                .collect()
-        })
-        .collect::<Vec<Vec<char>>>();
+            let coords = (row, col);
+
+            let up = if coords.0 > 0 {
+                Some((coords.0 - 1, coords.1))
+            } else {
+                None
+            };
+            let down = if coords.0 < rows.len() - 1 {
+                Some((coords.0 + 1, coords.1))
+            } else {
+                None
+            };
+            let left = if coords.1 > 0 {
+                Some((coords.0, coords.1 - 1))
+            } else {
+                None
+            };
+            let right = if coords.1 < line.len() - 1 {
+                Some((coords.0, coords.1 + 1))
+            } else {
+                None
+            };
+
+            graph.insert(
+                coords,
+                Node {
+                    coords,
+                    char: c,
+                    up,
+                    down,
+                    left,
+                    right,
+                },
+            );
+        }
+    }
 
     HeightMapData {
         graph: graph,
         src: src,
         dest: dest,
-        initial_unvisited: unvisited,
-        initial_tentative_distances: tenative_distances,
     }
 }
 
 fn part1(input: String) -> usize {
     let data = graph_from_input(input);
-    let dest = data.dest.clone();
-    let tentative = find_paths(data);
-
-    *tentative.get(&dest).unwrap()
+    let src = data.src.clone();
+    find_shortest_path(data, src)
 }
 
 fn part2(input: String) -> usize {
-    0
+    let data = graph_from_input(input.clone());
+
+    let mut min_distance = INFINITY;
+
+    let starting_points: Vec<&Node> = data
+        .graph
+        .values()
+        .filter(|node| get_char_value(node.char) == 'a')
+        .collect();
+
+    for (index, node) in starting_points.iter().enumerate() {
+        println!("{}/{}", index, starting_points.len());
+        let data = graph_from_input(input.clone());
+        let distance = find_shortest_path(data, node.coords);
+        min_distance = if distance < min_distance {
+            distance
+        } else {
+            min_distance
+        };
+    }
+
+    min_distance
 }
 
 fn main() {
@@ -173,5 +202,5 @@ fn test_part1() {
 #[test]
 fn test_part2() {
     let input = fs::read_to_string("./test-input.txt").expect("Error!");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 29);
 }
